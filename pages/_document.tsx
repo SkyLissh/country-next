e584d167d62e63535256ef1ecf24b7b93b5ab995
Alisson Lopez <alisson.hdz.lopez@hotmail.com>
2022-07-19 15:00:48 -0500

    :mag: Meta Tags
    Add meta tags fror improve SEO, and web sharing

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,20 +4,45 @@ export default function Document() {
 	return (
 		<Html lang="en">
 			<Head>
+				<meta charSet="UTF-8" />
 				<link rel="icon" href="/favicon.ico" />
 
-				<meta charSet="UTF-8" />
 				<meta name="author" content="SkyLissh" />
 				<meta
 					name="description"
 					content="A page to show the use case of Country API, the main idea is from FrontendMentor"
 				/>
+				<meta
+					name="theme-color"
+					media="(prefers-color-scheme: light)"
+					content="#ffffff"
+				/>
+				<meta name="theme-color" media="(prefers-color-scheme: dark)" content="#2B3945" />
+
+				{/* Opengraph  */}
+				<meta property="og:type" content="website" />
+				<meta
+					property="og:description"
+					content="A page to show the use case of Country API, the main idea is from FrontendMentor"
+				/>
+				<meta property="og:url" content="https://country.skylissh.me" />
+				<meta property="og:image" content="/desktop-preview.jpg" />
+
+				{/* Twitter */}
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@SkyLissh" />
+				<meta name="twitter:creator" content="@SkyLissh" />
+				<meta
+					name="twitter:description"
+					content="A page to show the use case of Country API, the main idea is from FrontendMentor"
+				/>
+				<meta name="twitter:image:src" content="/desktop-preview.jpg" />
 
 				{/* Google Fonts */}
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 				<link
-					href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=optional"
+					href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;600;800&display=swap"
 					rel="stylesheet"
 				></link>
 			</Head>
